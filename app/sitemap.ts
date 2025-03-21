c48516b27d2e09942587a4f3587799cc94974f26
Puurks <96667467+Puurks@users.noreply.github.com>
2025-03-21 15:08:42 -0500

    .net

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://miracat.com/',
+      url: 'https://miracat.net/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     // {
-    //   url: 'https://miracat.com/projects/home.html',
+    //   url: 'https://miracat.net/projects/home.html',
     //   lastModified: new Date(),
     //   changeFrequency: 'monthly',
     //   priority: 0.5,
